adcc1a7eb626a892a753c64b121853ee7848120d
chubbyhippo <mkmsacc@outlook.com>
2025-12-17 12:11:56 +0700

    Add `SimpleFrameDemo` with a basic Swing application setup

---
+++ src/main/java/io/github/chubbyhippo/v2ch10/simpleframe/SimpleFrameDemo.java
@@ -0,0 +1,22 @@
+package io.github.chubbyhippo.v2ch10.simpleframe;
+
+import module java.desktop;
+
+class SimpleFrameDemo {
+    void main() {
+        EventQueue.invokeLater(() -> {
+            var frame = new SimpleFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class SimpleFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public SimpleFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
